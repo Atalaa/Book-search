44fa8964c0d01788cc756b606cbbcdbab4265da3
Atala <atala.rchidi@gmail.com>
2020-05-22 11:41:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+
+  const inputRef  = useRef(null);
+  const searchRef = useRef(null);
+  console.log(inputRef);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  function searchKeyDown(e){
+    if(e.key === "Enter"){
+      searchRef.current.focus();
+    }
+  }
+
+  function submitKeyDown(){
+    alert('Submitted');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Search your book</h1>
+        <input type="text" onKeyDown={searchKeyDown}  ref={inputRef} id="name" name="name" required placeholder="Search a book..." minLength="1" maxLength="40" size="30"/>
       </header>
+      <button onKeyDown={submitKeyDown} ref={searchRef}>Search</button>
     </div>
   );
 }
